15150ce5b4a2ce9fa3bd6d0845f1aa8079a45f35
public-jun <generalpublicjunjun@gmail.com>
2021-12-13 18:32:13 +0900

    minor update

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,9 +1,26 @@
 #include <Board.hpp>
+#include <Player.hpp>
 
 int main(void)
 {
 	Board board;
+	Player first(Player::kFirst);
+	Player second(Player::kLast);
+	// Player &tmp = first;
+
+	bool is_continue = true;
 
 	board.printBoard();
+	while (is_continue)
+	{
+		//入力を求める
+		std::string input;
+		std::cout << "行番号(1 ~ 7)を入力してください" << std::endl;
+		if (!std::getline(std::cin, input))
+			break;
+		//Boardにセット
+
+		//表示
+	}
 	return (0);
 }
